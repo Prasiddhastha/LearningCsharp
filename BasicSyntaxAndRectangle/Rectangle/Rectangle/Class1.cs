aa7f6a48e8650211a83d9b6ccfca04dbb2c0441a
Sweaty Zeno <Sweaty Zeno@DESKTOP-BGTK4AE>
2022-12-05 16:14:56 +0545

    Add project files.

---
+++ BasicSyntaxAndRectangle/Rectangle/Rectangle/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        double length, breadth;
+
+        public void GetInput()
+        {
+            Console.Write("Enter Length:");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Breadth:");
+            breadth = Convert.ToInt32(Console.ReadLine());
+        }
+        public double RectArea()
+        {
+            return length * breadth;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Length:{length}\nBreadth:{breadth}");
+            Console.WriteLine($"Area is: {RectArea()}");
+        }
+    }
+}
---
+++ BasicSyntaxAndRectangle/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+
+namespace RectangleApplication
+{
+    
+    
+       
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.GetInput();
+            r.display();
+        }
+
+    }
+
+}
+
+
+
+
+
